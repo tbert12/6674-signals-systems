7d32ae2f2473b9db1647fd794a095ae8860fdcca
tbert <tomasbert12@gmail.com>
2018-06-13 16:44:33 -0300

    Primero 4 ejercicios terminados

---
+++ tp/dummy_plot.py
@@ -0,0 +1,53 @@
+from scipy.io import wavfile
+from tp.vocal import Letter
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+def ej1():
+    (FS, Y) = wavfile.read("res/hh15.WAV")
+
+    # Duracion
+    L = len(Y) / FS
+    print("Rate: {} | Muestras: {} | Duracion {}s | Duracion/Muestras = {}".format(FS, len(Y), L, L / len(Y)))
+
+    X = np.arange(0, len(Y)) / FS
+    Y = Y / max(Y)  # Y normalizado
+    plt.title(r'Señal de audio con fonemas', fontsize=20)
+    plt.plot(X, Y)
+    plt.xticks(np.arange(min(X), max(X), 0.2))
+
+    UMBRAL = 0.05
+    inicia_mudo = False
+    for x, v in zip(X, Y):
+        v = abs(v)
+        if inicia_mudo and v > UMBRAL:
+            plt.axvline(x=x, alpha=0.05, color="red")
+            inicia_mudo = False
+        if v <= UMBRAL and not inicia_mudo:
+            # plt.axvline(x=x, linestyle='dotted', alpha=0.2, color="red")
+            inicia_mudo = True
+
+    red_patch = mpatches.Patch(color='red', label='Sonoros')
+    white_patch = mpatches.Patch(color='white', label='Sordos')
+    plt.legend(handles=[red_patch, white_patch])
+
+    plt.xlabel('tiempo (s)')
+    plt.ylabel('intensidad')
+
+    for l in Letter.LETTERS_POSITION:
+        y = 0.9
+        x = l.start + 0.009
+        plt.text(x, y, l.char)
+        plt.axvline(x=l.start, color="yellow")
+
+    plt.show()
+
+
+def main():
+    ej1()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tp/vocal/Letter.py
@@ -0,0 +1,35 @@
+class Letter:
+    def __init__(self, char='', start=0.0, end=0.0):
+        self.char = char.upper()
+        self.start = start
+        self.end = end
+
+
+LETTERS_POSITION = [
+    # Por Gastón
+    Letter('c',	0.5544,	0.5666),
+    Letter('o',	0.5788,	0.6216),
+    Letter('n',	0.6644,	0.8660),
+    Letter('t',	0.9216,	0.9316),
+    Letter('a',	0.9416,	0.9954),
+    Letter('g',	1.0492,	1.2076),
+    Letter('i',	1.2076,	1.2444),
+    Letter('a',	1.2812,	1.3712),
+
+    Letter('e',	1.3712,	1.4262),
+    Letter('n',	1.4812,	1.5168),
+    Letter('e',	1.5524,	1.6142),
+    Letter('r',	1.6760,	1.7190),
+    Letter('g',	1.7620,	1.8352),
+    Letter('i',	1.9084,	1.9858),
+    Letter('a',	2.0632,	2.1648),
+
+    Letter('y',	2.1648,	2.2924),
+
+    Letter('f',	2.2924,	2.3702),
+    Letter('e',	2.4480,	2.4982),
+    Letter('r',	2.5484,	2.6332),
+    Letter('v',	2.7180,	2.7554),
+    Letter('o',	2.7928,	2.8690),
+    Letter('r',	2.9452,	3.1292),
+]
